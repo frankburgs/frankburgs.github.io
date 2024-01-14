a9eccb3f35c66ba318c7d0f80ca84577157d3f67
frankburgs <franciscoburgers@gmail.com>
2024-01-14 12:16:58 -0800

    Added safety list in progress

---
+++ observations.js
@@ -0,0 +1,16 @@
+// List safety observations at the weld school.
+
+// Variable name convention for observations, generalSpecific (?)
+
+
+labelFlammable = {
+  observation: "Flammable cabinet not labeled",
+  recommendation: "Label it with this linked product",
+  reference: "Ecfr",
+  media: "img",
+}
+
+
+
+labels = [labelFlammable]
+
---
+++ script.js
@@ -0,0 +1,5 @@
+/*
+ Try to only manipulate ID's instead of classes to not conflict with Bootstrap
+*/
+
+console.log(labels)
