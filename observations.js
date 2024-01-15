20fbcaa5140c11038d861e17dff28c4ac2712289
frankburgs <franciscoburgers@gmail.com>
2024-01-14 17:09:54 -0800

    Added images, working on table

--- observations.js
+++ observations.js
@@ -1,14 +1,26 @@
-// List safety observations at the weld school.
+/* List safety observations at the weld school.
 
-// Variable name convention for observations, generalSpecific (?)
+Variable name convention for observations, generalSpecific
 
+generalSpecific = {
+  observation: "",
+  recommendation: "",
+  reference: "",
+  link: "",
+  media: "",
+}
+
+*/
 
 labelFlammable = {
   observation: "Flammable cabinet not labeled",
   recommendation: "Label it with this linked product",
-  reference: "Ecfr",
-  media: "img",
+  reference: "29 CFR 1926.152(b)(2)(iii)",
+  media: "labelFlammableCabinet",
+  link: "https://www.ecfr.gov/current/title-29/part-1926/section-1926.152#p-1926.152(b)(2)(iii)",
 }
+
+
 
 
 
