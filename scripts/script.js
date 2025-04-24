4afa58f0bfcdd5affcaccfcd6cc787ef0f5d49ed
frankburgs <franciscoburgers@gmail.com>
2025-04-23 20:43:39 -0700

    Reactifying

--- scripts/script.js
+++
@@ -1,9 +0,0 @@
-var tableBody = document.getElementById("tableBody")
-
-var counter = 1; // Counter to match button to modal ID's and count line items.
-
-const sortedObservations = sortObservations(); // Get the sorted list of observations.
-
-for (observation in sortedObservations){ // Populate the table with observations.
-  populateRow(sortedObservations[observation])
-}
