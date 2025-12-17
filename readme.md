61d2086e569307d417b130b9fb59f3d1d6a210ad
frankburgs <franciscoburgers@gmail.com>
2025-12-17 14:09:08 -0800

    router not routing

--- readme.md
+++ readme.md
@@ -5,6 +5,9 @@
 - I move the index.html and assets folder out of dist folder and into root, these contain hashed files.
 - Delete dist folder.
 
+- Change to HashRouter & Router
+- add to vite.config.js  base: "/frankburgs.github.io/"
+- add to package.json "homepage": "frankburgs.github.io",
 
 # Current tasks
 - Shorten & fix :
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from "react-router";
+import { HashRouter as Router, Routes, Route } from "react-router";
 import App from './App.jsx'
 import Noise from './Noise.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <Router>
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/noise" element={<Noise />} />
     </Routes>
-  </BrowserRouter>,
+  </Router>,
 )
